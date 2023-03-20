b3aeec426c16e7c79f5a77c0126e370a576f763a
lyc-Lacewing <lycrlsu01@gmail.com>
2023-03-20 12:58:00 +0800

    aa

--- Core/LaCursor.cs
+++ Core/LaCursor.cs
@@ -39,5 +39,74 @@ namespace tLaText.Core
             CursorColor = cursorColor;
             SelectionColor = selectionColor;
         }
+
+        /// <summary>
+        /// Order the cursors in ascending order.
+        /// </summary>
+        private void OrderCursor()
+        {
+            Cursors = Cursors.OrderBy(c => c.Cursor).ToList();
+        }
+        /// <summary>
+        /// Checks if cursors at c1 and c2 overlap.
+        /// </summary>
+        /// <param name="c1"></param>
+        /// <param name="c2"></param>
+        /// <returns></returns>
+        private bool CursorOverlap(int c1, int c2)
+        {
+            return Cursors[c1].Selection.Overlap(Cursors[c2].Selection);
+        }
+        /// <summary>
+        /// Finds where should cursor of the merged cursor be.
+        /// </summary>
+        /// <param name="c1"></param>
+        /// <param name="c2"></param>
+        /// <returns></returns>
+        private int FindMergedCursor(int c1, int c2)
+        {
+            Range selection = Cursors[c1].Selection.MergeWith(Cursors[c2].Selection);
+            if (Cursors[c1].Cursor == selection.Start.Value)
+            {
+                return selection.Start.Value;
+            }
+            if (Cursors[c1].Cursor == selection.End.Value)
+            {
+                return selection.Start.Value;
+            }
+            if (Cursors[c2].Cursor == selection.Start.Value)
+            {
+                return selection.Start.Value;
+            }
+            if (Cursors[c2].Cursor == selection.End.Value)
+            {
+                return selection.Start.Value;
+            }
+            return selection.Start.Value;
+        }
+        private void MergeOverlaps()
+        {
+            for (int i = 0; i < Cursors.Count - 1; i++)
+            {
+                for (int j = i + 1; j < Cursors.Count; j++)
+                {
+                    if (!CursorOverlap(i, j))
+                    {
+                        continue;
+                    }
+                    Cursors[i].
+                }
+            }
+        }
+
+        /// <summary>
+        /// Add a cursor then reorder the cursors.
+        /// </summary>
+        /// <param name="cursor"></param>
+        public void AddCursor(ACursor cursor)
+        {
+            Cursors.Add(cursor);
+            OrderCursor();
+        }
     }
 }
---
+++ Core/Utils.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tLaText.Core
+{
+    internal static class Utils
+    {
+        /// <summary>
+        /// Checks if the Range overlaps with the other Range.
+        /// </summary>
+        /// <param name="r"></param>
+        /// <param name="range"></param>
+        /// <returns></returns>
+        public static bool Overlap(this Range r, Range range)
+        {
+            if (r.Start.Value <= range.Start.Value)
+            {
+                if (r.Start.Value >= range.Start.Value)
+                {
+                    return true;
+                }
+            }
+            if (r.Start.Value > range.Start.Value)
+            {
+                if (r.Start.Value <= range.End.Value)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        /// <summary>
+        /// Merges the Range with the other Range.
+        /// </summary>
+        /// <param name="r"></param>
+        /// <param name="range"></param>
+        public static Range MergeWith(this Range r, Range range)
+        {
+            return new Range(Math.Min(r.Start.Value, range.Start.Value), Math.Max(r.End.Value, range.End.Value));
+        }
+    }
+}
