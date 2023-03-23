9fd3a15c28e093f734010f801d7ec48b86b731d6
lyc-Lacewing <lycrlsu01@gmail.com>
2023-03-23 14:28:18 +0800

    get type

--- Core/LaTextParser.cs
+++ Core/LaTextParser.cs
@@ -1,104 +1,90 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace tLaText.Core
 {
+    public struct EscapingText
+    {
+        public char[] input;
+        public string output;
+
+        public EscapingText(char[] input, string output)
+        {
+            this.input = input;
+            this.output = output;
+        }
+
+        public bool TryMatch(char input, out string output)
+        {
+            output = string.Empty;
+            if (!this.input.Contains(input))
+            {
+                return false;
+            }
+            output = this.output;
+            return true;
+        }
+    }
     internal class LaTextParser
     {
-        internal enum Parsing
+        public enum ArgType
         {
-            /// <summary>
-            /// Parsing starts.
-            /// </summary>
-            TextStart,
-            /// <summary>
-            /// Text is a literal.
-            /// </summary>
-            Literal,
-            /// <summary>
-            /// Text is a whitespace.
-            /// </summary>
-            Space,
-            /// <summary>
-            /// Text is a separator for literals.
-            /// </summary>
-            LitSeparator,
-            /// <summary>
-            /// Text is a trigger of commands.
-            /// </summary>
-            CommandTrigger,
-            /// <summary>
-            /// Text is command.
-            /// </summary>
             Command,
-            /// <summary>
-            /// Text is a separator for commands.
-            /// </summary>
-            CmdSeparator,
-            /// <summary>
-            /// Text is a parameter.
-            /// </summary>
-            Parameter,
-            /// <summary>
-            /// Text is a separator for parameters.
-            /// </summary>
-            ParaSeparator,
-            /// <summary>
-            /// Text is a escaper.
-            /// </summary>
+            Text
+        }
+        public enum CharType
+        {
+            Literal,
             Escaper,
-            /// <summary>
-            /// Text is escaped.
-            /// </summary>
             Escapee,
-            /// <summary>
-            /// Text is an inline formatter.
-            /// </summary>
             Formatter,
-            /// <summary>
-            /// Text is formatted.
-            /// </summary>
             Formattee,
-            /// <summary>
-            /// Parsing ends.
-            /// </summary>
-            TextEnd
+            CmdTrigger,
+            Command,
+            CmdSeparator,
+            Parameter,
+            ParaSeparator
         }
-        private static Parsing[] LeagalNextInputStates(Parsing state)
-        {
-            switch (state)
-            {
-                default: 
-                    return new Parsing[] { Parsing.TextEnd };
-                case Parsing.TextStart:
-                    return new Parsing[] { Parsing.Literal, Parsing.Space, Parsing.CommandTrigger, Parsing.Escaper, Parsing.Formatter, Parsing.TextEnd };
-                case Parsing.Literal:
-                    return new Parsing[] { Parsing.Literal, Parsing.Space, Parsing.Escaper, Parsing.Formatter, Parsing.TextEnd };
-                case Parsing.Space:
-                    return new Parsing[] { Parsing.Literal, Parsing.CommandTrigger, Parsing.Escaper, Parsing.Formatter, Parsing.TextEnd };
-                case Parsing.CommandTrigger:
-                    return new Parsing[] { Parsing.Command };
-                case Parsing.Command:
-                    return new Parsing[] { Parsing.Space, Parsing.CmdSeparator, Parsing.TextEnd };
-                case Parsing.CmdSeparator:
-                    return new Parsing[] { Parsing.Parameter };
-                case Parsing.Parameter:
-                    return new Parsing[] { Parsing.Space, Parsing.ParaSeparator, Parsing.TextEnd };
-                case Parsing.ParaSeparator:
-                    return new Parsing[] { Parsing.Parameter };
-                case Parsing.Escaper:
-                    return new Parsing[] { Parsing.Escapee };
-                case Parsing.Formatter:
-                    return new Parsing[] { Parsing.Literal, Parsing.Space };
-            }
-        }
-        private static Parsing NextState(Parsing previous, Parsing current)
-        {
+    }
+    public static class SpecT
+    {
+        public static readonly char[] Escaper = new char[] { '\\' };
+        public static readonly char[] CmdTrigger = new char[] { '.', '。' };
+        public static readonly char[] Formatter = new char[] { '*' };
+        public static readonly char[] SpacePlaceholder = new char[] { '_' };
+    }
+    public static class EscT
+    {
+        public static readonly EscapingText LineFeed = new EscapingText(new char[] { 'n', 'r' }, "\n");
+        public static readonly EscapingText Tab = new EscapingText(new char[] { 't' }, "\t");
 
-            return Parsing.TextEnd;
+        private static EscapingText[] GetEscTexts()
+        {
+            var fields = Array.FindAll(typeof(EscT).GetFields(BindingFlags.Public | BindingFlags.Static), t => t.IsInitOnly && t.DeclaringType == typeof(EscapingText));
+            var result = new List<EscapingText>();
+            Array.ForEach(fields, field =>
+            {
+                var value = field.GetValue(null);
+                result.Add((EscapingText)value);
+            });
+            return result.ToArray();
+        }
+
+        public static string Escape(char escapee)
+        {
+            EscapingText[] escapingTexts = GetEscTexts();
+            for (int i = 0; i < escapingTexts.Length; i++)
+            {
+                if (escapingTexts[i].TryMatch(escapee, out string output))
+                {
+                    return output;
+                }
+            }
+            return string.Empty;
         }
     }
 }
